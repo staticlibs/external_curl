472608dbabc1bacf5828df7cff8423a729528787
alex <alex@staticlibs.net>
2015-02-22 13:19:12 +0000

    linux, raspberry and android build work

--- test/curl_test.cpp
+++ test/curl_test.cpp
@@ -18,7 +18,13 @@ int main() {
     CURL* curl = curl_easy_init();
     assert(curl);
     curl_easy_setopt(curl, CURLOPT_URL, "https://google.com");
+    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0);
+    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0);
     auto res = curl_easy_perform(curl);
+    if (CURLE_OK != res) {
+        puts(curl_easy_strerror(res));
+    } 
+    curl_easy_cleanup(curl);
     assert(CURLE_OK == res);
 
     return 0;

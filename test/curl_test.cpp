c3810cc0ab21d9db4f11eca9a8b49e0b445907f7
alex <alex@staticlibs.net>
2015-02-22 01:35:52 +0000

    linux amd64 works

--- test/curl_test.cpp
+++ test/curl_test.cpp
@@ -5,8 +5,21 @@
  * Created on February 10, 2015, 11:25 AM
  */
 
+
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cassert>
+
+#include "curl/curl.h"
+
+
 int main() {
+    CURL* curl = curl_easy_init();
+    assert(curl);
+    curl_easy_setopt(curl, CURLOPT_URL, "https://google.com");
+    auto res = curl_easy_perform(curl);
+    assert(CURLE_OK == res);
 
     return 0;
 }
-
